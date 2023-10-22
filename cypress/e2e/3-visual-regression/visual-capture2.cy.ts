ee7b48357378dce802b2cf42e84070d5be7e01ed
kishors33 <kishors33@gmail.com>
2023-10-22 16:59:46 +0100

    updated tolerances in tests

--- cypress/e2e/3-visual-regression/visual-capture2.cy.ts
+++ cypress/e2e/3-visual-regression/visual-capture2.cy.ts
@@ -13,6 +13,7 @@ refer visual-capture3.cy.ts  */
 describe.only('Visual Capture Test', () => {
     it('should compare screenshot of the entire page', () => {
       cy.visit('https://github.com/kishors33?tab=repositories');
-      cy.compareSnapshot('home-page');
+      // cy.compareSnapshot('home-page');
+      cy.compareSnapshot('home-page', 0.2);
     })
   })
--- cypress/e2e/3-visual-regression/visual-capture3.cy.ts
+++ cypress/e2e/3-visual-regression/visual-capture3.cy.ts
@@ -8,9 +8,9 @@
    b) If you start the Cypress run again by running npx cypress open again, then the comparison as well as diff folders are emptied first 
    (the baseline folder is not emptied) and then new files are written to both comparision and diff folder when there is a failure */
 
-/* If you compare an image which does not change with time then the test passes each time */
+/* If you compare an image which does not change with time then the test passes each time - refer visual-capture3.cy.ts  */
 
-describe.only('Visual Capture Test', () => {
+   describe.only('Visual Capture Test', () => {
     it('should compare screenshot of the entire page', () => {
       cy.visit('https://github.com/kishors33?tab=projects');
       cy.compareSnapshot('home-page');
