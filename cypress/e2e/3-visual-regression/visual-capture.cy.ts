9d765744a4e4a80de5943fc35a31bbb054666479
kishors33 <kishors33@gmail.com>
2023-10-22 15:20:44 +0100

    updated 3 tests to avoid running all tests in pipeline

--- cypress/e2e/3-visual-regression/visual-capture.cy.ts
+++ cypress/e2e/3-visual-regression/visual-capture.cy.ts
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe('Visual Capture Test', () => {
+describe.only('Visual Capture Test', () => {
     it('should compare screenshot of the entire page', () => {
       cy.visit('https://github.com/kishors33');
       cy.compareSnapshot('home-page');
